86c05b8520be9b20850a3843ca8f8e1f4c964324
NamanJava <aroranaman048@gmail.com>
2023-08-18 02:36:24 +0530

    main

---
+++ Git2.java
@@ -0,0 +1,14 @@
+class Git 
+{
+ public static void main(String [] args)
+{
+System.out.println("This file contain git repo");
+}
+}
+class Git2
+{
+ public static void main(String [] args)
+{
+System.out.println("This file contain 2 git repo");
+}
+}
